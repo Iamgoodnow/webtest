a864933173ff467931ca9a8651c477a15d665d47
cica@github.com <anemes@darkestfuture.local>
2024-01-04 12:59:16 +0100

    fixing form handling

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 console.log("Console log");
 window.alert("This is a window alert.");
 let age = "69";
+let textbox
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
 console.log(age);
-while (1=1) {
-    let formd1 = document.getElementById("t1")
-    if (formd1.length > 10) {console.log(formd1)}
-
-}
+while (1=1){
+    let textbox = document.getElementById("t1").value;
+    if (formd1.length > 10){console.log(formd1)};
+    sleep(1000);
+    }
