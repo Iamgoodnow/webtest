64912298e8a45ae19aa8d866109420995200b10a
cica@github.com <anemes@darkestfuture.local>
2024-01-04 12:41:56 +0100

    added form handling

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ console.log("Console log");
 window.alert("This is a window alert.");
 let age = "69";
 console.log(age);
+while (1=1) {
+    let formd1 = document.getElementById("t1")
+    if (formd1.length > 10) {console.log(formd1)}
+
+}
