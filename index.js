0382aeb387b4f667ecbe20e6db28ff4919955b71
cica@github.com <anemes@darkestfuture.local>
2024-01-04 14:42:41 +0100

    error patch test

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-console.log("Console log");
-window.alert("This is a window alert.");
-let age = "69";
-let textbox;
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  };
-console.log(age);
-while (1=1){
-    let textbox = document.getElementById("t1").value;
-    if (formd1.length > 10){console.log(formd1)};
-    sleep(1000);
-};
+//console.log("Console log");
+//window.alert("This is a window alert.");
+//let age = "69";
+//let textbox;
+//console.log(age);
+let text;
+while(true){
+    setTimeout(function(){
+        text = document.getElementById("t1").value;
+        if(text != ""){
+            console.log("True")
+        }    
+    }, 2000);
+}
